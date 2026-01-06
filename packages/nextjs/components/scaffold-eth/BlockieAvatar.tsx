c413274126f31327158f2d9c3de87b93de33a5ff
Shane Pearson <bubbafoley@gmail.com>
2026-01-06 00:11:52 -0600

    Fix nextjs lint errors

--- packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
+++ packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
@@ -6,7 +6,6 @@ import { blo } from "blo";
 // Custom Avatar for RainbowKit
 export const BlockieAvatar: AvatarComponent = ({ address, ensImage, size }) => (
   // Don't want to use nextJS Image here (and adding remote patterns for the URL)
-  // eslint-disable-next-line @next/next/no-img-element
   <img
     className="rounded-full"
     src={ensImage || blo(address as `0x${string}`)}
